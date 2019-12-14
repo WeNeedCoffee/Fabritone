a5a2090e1c90ef0a86c4bc1171f52a741311a778
CDAGaming <cstack2011@yahoo.com>
2019-12-13 22:29:27 -0600

    [Port-Commit] Port origin/0db4b19
    Ref: https://github.com/cabaletta/baritone/commit/0db4b193e14b5df0990b5c65c512a72399da534c

--- src/main/java/baritone/command/defaults/GotoCommand.java
+++ src/main/java/baritone/command/defaults/GotoCommand.java
@@ -49,9 +49,13 @@ public class GotoCommand extends Command {
             customGoalProcess.path();
             logToast("Going to current goal");
         }
-        if (args.peekDatatypeOrNull(RelativeCoordinate.INSTANCE) != null) { // if we have a numeric first argument...
+        // If we have a numeric first argument, then parse arguments as coordinates.
+        // Note: There is no reason to want to go where you're already at so there
+        // is no need to handle the case of empty arguments.
+        if (args.peekDatatypeOrNull(RelativeCoordinate.INSTANCE) != null) {
+            args.requireMax(3);
             BetterBlockPos origin = baritone.getPlayerContext().playerFeet();
-            Goal goal = args.getDatatypePostOrNull(RelativeGoal.INSTANCE, origin);
+            Goal goal = args.getDatatypePost(RelativeGoal.INSTANCE, origin);
             logToast(String.format("Going to: %s", goal.toString()));
             baritone.getCustomGoalProcess().setGoalAndPath(goal);
             return;
