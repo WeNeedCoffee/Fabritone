2c9842645186f05a647bdbdfd26de793344aebbd
CDAGaming <cstack2011@yahoo.com>
2020-01-02 20:52:28 -0600

    [Port-Commit] Port origin/812bc0d8
    Reference: https://github.com/cabaletta/baritone/commit/812bc0d8c40ebe56cd637280e0d412190d1f4f7d

--- src/main/java/baritone/utils/schematic/format/defaults/SpongeSchematic.java
+++ src/main/java/baritone/utils/schematic/format/defaults/SpongeSchematic.java
@@ -15,23 +15,18 @@
  * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package baritone.utils.schematic.parse;
+package baritone.utils.schematic.format.defaults;
 
-import baritone.api.schematic.parse.ISchematicParser;
 import baritone.utils.schematic.StaticSchematic;
-import baritone.utils.schematic.format.DefaultSchematicFormats;
 import baritone.utils.type.VarInt;
 import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtIo;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -39,82 +34,59 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * An implementation of {@link ISchematicParser} for {@link DefaultSchematicFormats#SPONGE}
- *
  * @author Brady
- * @since 12/16/2019
+ * @since 12/27/2019
  */
-public enum SpongeParser implements ISchematicParser {
-    INSTANCE;
+public final class SpongeSchematic extends StaticSchematic {
 
-    @Override
-    public StaticSchematic parse(InputStream input) throws IOException {
-        CompoundTag nbt = NbtIo.readCompressed(input);
-        int version = nbt.getInt("Version");
-        switch (version) {
-            case 1:
-            case 2:
-                return new SpongeSchematic(nbt);
-            default:
-                throw new UnsupportedOperationException("Unsupported Version of a Sponge Schematic");
+    public SpongeSchematic(CompoundTag nbt) {
+        this.x = nbt.getInt("Width");
+        this.y = nbt.getInt("Height");
+        this.z = nbt.getInt("Length");
+        this.states = new BlockState[this.x][this.z][this.y];
+
+        Int2ObjectArrayMap<BlockState> palette = new Int2ObjectArrayMap<>();
+        CompoundTag paletteTag = nbt.getCompound("Palette");
+        for (String tag : paletteTag.getKeys()) {
+            int index = paletteTag.getInt(tag);
+
+            SerializedBlockState serializedState = SerializedBlockState.getFromString(tag);
+            if (serializedState == null) {
+                throw new IllegalArgumentException("Unable to parse palette tag");
+            }
+
+            BlockState state = serializedState.deserialize();
+            if (state == null) {
+                throw new IllegalArgumentException("Unable to deserialize palette tag");
+            }
+
+            palette.put(index, state);
         }
-    }
 
-    /**
-     * Implementation of the Sponge Schematic Format supporting both V1 and V2. (For the current
-     * use case, there is no difference between reading a V1 and V2 schematic).
-     */
-    private static final class SpongeSchematic extends StaticSchematic {
-
-        SpongeSchematic(CompoundTag nbt) {
-            this.x = nbt.getInt("Width");
-            this.y = nbt.getInt("Height");
-            this.z = nbt.getInt("Length");
-            this.states = new BlockState[this.x][this.z][this.y];
-
-            Int2ObjectArrayMap<BlockState> palette = new Int2ObjectArrayMap<>();
-            CompoundTag paletteTag = nbt.getCompound("Palette");
-            for (String tag : paletteTag.getKeys()) {
-                int index = paletteTag.getInt(tag);
-
-                SerializedBlockState serializedState = SerializedBlockState.getFromString(tag);
-                if (serializedState == null) {
-                    throw new IllegalArgumentException("Unable to parse palette tag");
-                }
-
-                BlockState state = serializedState.deserialize();
-                if (state == null) {
-                    throw new IllegalArgumentException("Unable to deserialize palette tag");
-                }
-
-                palette.put(index, state);
+        // BlockData is stored as an NBT byte[], however, the actual data that is represented is a varint[]
+        byte[] rawBlockData = nbt.getByteArray("BlockData");
+        int[] blockData = new int[this.x * this.y * this.z];
+        int offset = 0;
+        for (int i = 0; i < blockData.length; i++) {
+            if (offset >= rawBlockData.length) {
+                throw new IllegalArgumentException("No remaining bytes in BlockData for complete schematic");
             }
 
-            // BlockData is stored as an NBT byte[], however, the actual data that is represented is a varint[].
-            byte[] rawBlockData = nbt.getByteArray("BlockData");
-            int[] blockData = new int[this.x * this.y * this.z];
-            int offset = 0;
-            for (int i = 0; i < blockData.length; i++) {
-                if (offset >= rawBlockData.length) {
-                    throw new IllegalArgumentException("No remaining bytes in BlockData for complete schematic");
-                }
+            VarInt varInt = VarInt.read(rawBlockData, offset);
+            blockData[i] = varInt.getValue();
+            offset += varInt.getSize();
+        }
 
-                VarInt varInt = VarInt.read(rawBlockData, offset);
-                blockData[i] = varInt.getValue();
-                offset += varInt.getSize();
-            }
-
-            for (int y = 0; y < this.y; y++) {
-                for (int z = 0; z < this.z; z++) {
-                    for (int x = 0; x < this.x; x++) {
-                        int index = (y * this.z + z) * this.x + x;
-                        BlockState state = palette.get(blockData[index]);
-                        if (state == null) {
-                            throw new IllegalArgumentException("Invalid Palette Index " + index);
-                        }
-
-                        this.states[x][z][y] = state;
+        for (int y = 0; y < this.y; y++) {
+            for (int z = 0; z < this.z; z++) {
+                for (int x = 0; x < this.x; x++) {
+                    int index = (y * this.z + z) * this.x + x;
+                    BlockState state = palette.get(blockData[index]);
+                    if (state == null) {
+                        throw new IllegalArgumentException("Invalid Palette Index " + index);
                     }
+
+                    this.states[x][z][y] = state;
                 }
             }
         }
@@ -138,11 +110,11 @@ public enum SpongeParser implements ISchematicParser {
                 Block block = Registry.BLOCK.get(this.resourceLocation);
                 this.blockState = block.getDefaultState();
 
-                // Note: Sorting differs here in Fabritone, but should remain the same output
                 this.properties.keySet().stream().sorted(String::compareTo).forEachOrdered(key -> {
                     Property<?> property = null;
 
-                    for (Property<?> p : this.blockState.getProperties()) {
+                    // Note: Sorting differs here in Fabritone, but should get the same output
+                    for (Property<?> p : block.getDefaultState().getProperties()) {
                         if (p.getName().equals(key)) {
                             property = p;
                             break;
